61a534e9833c8272ce54c2194a2d7cff50d457de
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-03-18 15:21:56 -0300

    AICommentarie AI

--- AllAIInUnityImplementationSimple/Assets/BasicSctructure/Script/Core/BehaviourTree/BehaviourTree.cs
+++ AllAIInUnityImplementationSimple/Assets/BasicSctructure/Script/Core/BehaviourTree/BehaviourTree.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BehaviourTree : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -13,4 +13,13 @@ public class BehaviourTree : MonoBehaviour
     {
         
     }
+    //This is the main class for the behaviour tree.
+    //It will contain the root node of the tree.
+    //The tree will be traversed from the root node to the leaf nodes.
+    //Each node will return a status: success, failure or running.
+    //The tree will be traversed until a node returns running or failure.
+    //If a node returns success, the tree will continue to the next node.
+    //If a node returns failure, the tree will stop.
+    //If a node returns running, the tree will continue to the next frame.
+
 }
